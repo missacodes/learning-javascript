49e0b72e00ce7d8325bb9c85c625cf4e6bddd5bb
missacodes <issam337b@gmail.com>
2025-12-12 18:26:35 +0000

    new chapter!

---
+++ variables/let-and-const.js
@@ -0,0 +1,10 @@
+const name="Mohamed"
+const favcolor = "Black"
+let location="United Kingdom"
+let mood="Motivated"
+console.log(name)
+console.log(favcolor)
+console.log(location)
+console.log(mood)
+location="Europe"
+console.log(location)
