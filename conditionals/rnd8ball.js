124f2ec43898da46b4148b4339a4566fc7b4d76c
missacodes <issam337b@gmail.com>
2025-12-18 23:35:41 +0000

    completed this task

---
+++ conditionals/rnd8ball.js
@@ -0,0 +1,30 @@
+const number = (Math.floor(Math.random()*7));
+if (number===0)
+{   console.log ('Question: Am I a great programmer?');
+    console.log('Random 8 Ball: Yes - definitely');
+}
+if (number===1)
+{   console.log ('Question: Is Javascript better than Typescript?');
+    console.log('Random 8 Ball: It is decidedly so.');
+}
+else if (number===2)
+{   console.log ('Question: Was completing this chapter hard?');
+    console.log('Ask again later.');
+}
+
+else if (number===3)
+{   console.log ('Question: Does it get any harder than this?');
+    console.log('Better not tell you now.');
+}
+else if (number===4)
+{   console.log ('Question: Have you followed my Github?');
+    console.log('My sources say no.');
+}
+else if (number===5)
+{   console.log ('Question: What is my Name?');
+    console.log('Outlook not so good.');
+}
+else if (number === 6){
+    console.log ('Question: Should i quit learning Javascript?');
+    console.log('Very doubtful.');
+}
